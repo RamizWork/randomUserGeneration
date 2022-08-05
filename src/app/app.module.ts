911f76d0b54daeacb06d2b062094e6941d1f0cac
Ramiz <Ramizworking@gmail.com>
2022-08-05 14:04:02 +0500

    Completed basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentSectionComponent } from './content-section/content-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
